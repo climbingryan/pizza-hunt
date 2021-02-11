288f6c3ed48a7d26aee6391da1c0184990a81411
shaggy <simmons5family@gmail.com>
2021-02-11 16:49:18 -0600

    save pizza data to indexedDB

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -3,6 +3,7 @@ let db;
 // establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
 const request = indexedDB.open('pizza_hunt', 1);
 
+
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function(event) {
     // save reference to the database
@@ -22,8 +23,21 @@ request.onsuccess = function(event) {
 
     }
 };
-
+// error handeling
 request.onerror = function(event) {
     //log error here
     console.log(event.target.errorCode)
+}
+
+
+// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the db with read and write permissions
+    const transaction = db.transaction(['new_pizza'], 'readwrite');
+
+    // access the object store for 'new_pizza
+    const pizzaObjectStore = transaction.objectStore('new_pizza');
+
+    // add record to your store with add method
+    pizzaObjectStore.add(record);
 }
